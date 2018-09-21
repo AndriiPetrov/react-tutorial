48bb9f107e16b92b0e1599ebce425f3c46a081e5
Bratok3000 <bratok3000@gmail.com>
2018-09-21 11:57:49 +0300

    first commit of this project

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import Api from './Api';
+
+ReactDOM.render(
+    // <App />, 
+    <Api />,
+    document.getElementById('root')
+);
